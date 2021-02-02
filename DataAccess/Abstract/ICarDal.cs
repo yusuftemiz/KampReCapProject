2d078e38e5b3c5946b3919b075aec02afa119368
yusuf temiz <yusuftemiz22@gmail.com>
2021-02-03 00:37:57 +0300

    3.2.2021

--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -7,7 +7,7 @@ namespace DataAccess.Abstract
 {
     public interface ICarDal
     {
-        List<Car> GetById(int myId);
+        Car GetById(int myId);
         List<Car> GetAll();
         void Add(Car car);
         void Update(Car car);
